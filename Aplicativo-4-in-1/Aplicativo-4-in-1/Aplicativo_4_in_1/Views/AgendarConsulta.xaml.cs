52d21e598f85a77a3161acde7b335d7d8ee47a10
Ricardo Takuno <takunoricardo@gmail.com>
2017-09-06 22:37:58 -0300

    Aplicativo Agendar Consulta

---
+++ Aplicativo-4-in-1/Aplicativo-4-in-1/Aplicativo_4_in_1/Views/AgendarConsulta.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aplicativo_4_in_1.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AgendarConsulta : ContentPage
+	{
+        public string Nome { get; set; }
+        public DateTime DataAgendamento { get; set; }
+        public TimeSpan HoraAgendamento { get; set; }
+        
+		public AgendarConsulta ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+	}
+}
